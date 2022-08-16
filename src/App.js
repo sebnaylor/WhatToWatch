6450f61b6b8133d151b6c78b450e3e20655f73b6
Seb Naylor <sebnaylor@Sebs-MacBook-Air.local>
2022-08-16 16:55:26 +0100

    added routes, finished home page stying, added page folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
-import MediaHighlight from "./components/MediaHighlight";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
 import Nav from "./components/Nav";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <MediaHighlight />
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/media/:id" element={<>Media</>} />
+          <Route path="/dashboard" element={<>Dashboard</>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,64 @@
+import "./Home.css";
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div id="media-highlight" className="-mt-12 h-180 text-center">
+        <h2 className="text-center pt-80 text-2xl">Encanto</h2>
+        <div className="highlight-genres flex justify-center space-x-4 mt-4">
+          <p>Animation</p>
+          <p>Comedy</p>
+          <p>Family</p>
+        </div>
+        <p className="my-1">102 mins</p>
+        <div className="highlight-info flex items-center justify-center">
+          <div className="imdb flex mx-3">
+            <img className="object-contain mx-1" src="./imdb.png" alt="imdb" />
+            <p>7.3</p>
+          </div>
+          <div className="metacritic flex mx-3 my-8">
+            <img
+              className="object-contain mx-1"
+              src="./metacritic.png"
+              alt="metacritic"
+            />
+            <p>7.1</p>
+          </div>
+          <img
+            className="object-contain mx-3 w-6"
+            src="./info.png"
+            alt="info"
+          />
+        </div>
+      </div>
+      <div className="watchlist">
+        <div className="watchlist-header">
+          <div className="flex justify-between">
+            <h3 class="text-2xl mx-2">Watchlist</h3>
+            <div className="share flex items-center">
+              <h3>Shared</h3>
+              <img src="./filter.png" alt="" class="shared-icon mx-1 w-10" />
+            </div>
+          </div>
+        </div>
+        <div className="movies">
+          <h2 class="text-2xl mx-2 mb-4">Movies</h2>
+          <div className="movie-cards mx-2">
+            <div className="movie blank">
+              <div className="plus"></div>
+            </div>
+          </div>
+        </div>
+        <div className="series pb-5">
+          <h2 className="text-2xl mx-2 my-2">Series</h2>
+          <div className="movie-cards mx-2">
+            <div className="series blank">
+              <div className="plus"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
